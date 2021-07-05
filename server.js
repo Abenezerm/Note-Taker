b5206c6dbd873628fed261a11a21f90c02994422
Abenezer Merdassa <abenezermerdassa@gmail.com>
2021-07-04 22:11:27 -0500

    added html routes and a server file...

---
+++ server.js
@@ -0,0 +1,21 @@
+//dependencies..
+const express  = require('express');
+
+//configure express...
+const app = express();
+
+//initial port to listen on later...
+const PORT = process.env.PORT || 5000;
+
+//Sets up express to handle data parsing...
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Points to the "routes" for the server...
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+//Listener to start server...
+app.listen(PORT, () => {
+  console.log(`App listening on port: ${PORT}`);
+});
