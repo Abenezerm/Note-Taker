2037794746f01b1ffc5a925ad0601df4580488da
Abenezer Merdassa <abenezermerdassa@gmail.com>
2021-07-18 17:25:38 -0500

    fixed server error,

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 //Sets up express to handle data parsing...
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static('public'))
 //Points to the "routes" for the server...
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
